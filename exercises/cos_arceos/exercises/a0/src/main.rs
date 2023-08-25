389f09db197f02439b5dbfeee61837536efe88d2
Your Name <you@example.com>
2023-08-25 16:43:15 +0800

    labUbutun0825

--- exercises/cos_arceos/exercises/a0/src/main.rs
+++ exercises/cos_arceos/exercises/a0/src/main.rs
@@ -8,14 +8,19 @@ use axstd::println;
 
 // TODO: Implement macro println_prefix.
 #[cfg(feature = "axstd")]
-use axstd::println_prefix;
+// use axstd::println_prefix;
+macro_rules! println_prefix{
+    ($prefix:expr,$($arg:tt)*)=>{
+        println!(concat!($prefix,"{}"),$($arg)*);
+    };
+}
 
 #[cfg_attr(feature = "axstd", no_mangle)]
 fn main() {
     println!("Hello, world!");
 
-    let times = 2;
-    println_prefix!("Stdout: ", "Hello, world![{}]", times);
+    // let times = 2;
+    println_prefix!("Stdout: ", "Hello, world![{}]");
 
     println!("\n[ArceOS Tutorial]: A0 okay!");
 }
